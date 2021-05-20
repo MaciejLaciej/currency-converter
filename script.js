94a5246f312d45f971b7af85bca641d2bf1ffdc9
Maciej Łata <maciejcezarylata@gmail.com>
2021-05-20 21:44:17 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 console.log("cześć")
 
-const formElement = document.querySelector(".js-form");
-const selectCurrencyOut = document.querySelector(".js-currencyOut");
-const selectCurrencyIn = document.querySelector(".js-currencyIn");
+
+
+
+const calculation = (event) => {
+    event.preventDefault();
+
+    const selectCurrencyOut = document.querySelector(".js-currencyOut");
+    const selectCurrencyIn = document.querySelector(".js-currencyIn");
+    const outcome = document.querySelector(".js-outcome");
+    const userInput = document.querySelector(".js-userInput");
+    const amount = userInput.value;
+    const initialCurrency = selectCurrencyIn.value;
+    const finalCurrency = selectCurrencyOut.value;
+    let result = +rate(initialCurrency, finalCurrency) * amount;
+
+    if (initialCurrency === "NONE" || finalCurrency === "NONE") {
+        outcome.innerHTML = "Nie wybrano waluty";
+    } else {
+        outcome.innerHTML = `${amount} ${initialCurrency} = ${result.toFixed(
+            2
+        )} ${finalCurrency}`;
+    }
+};
+
 
 const rate = (initialCurrency, finalCurrency) => {
     const rateEUR = 4.5635;
@@ -12,43 +33,31 @@ const rate = (initialCurrency, finalCurrency) => {
             return 1 / rateEUR;
         case "PLN GBP":
             return 1 / rateGBP;
-            
+
         case "EUR PLN":
             return 1 * rateEUR;
-            
+
         case "GBP PLN":
             return 1 * rateGBP;
-            
+
         case "GBP EUR":
             return rateGBP / rateEUR;
-            
+
         case "EUR GBP":
             return rateEUR / rateGBP;
-            
+
     }
 };
 
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
+const cleaning = () => {
     const outcome = document.querySelector(".js-outcome");
-    const userInput = document.querySelector(".js-userInput");
-    const amount = userInput.value;
-    const initialCurrency = selectCurrencyIn.value;
-    const finalCurrency = selectCurrencyOut.value;
-    let result = +rate(initialCurrency, finalCurrency) * amount;
-    
-       if (initialCurrency === "NONE" || finalCurrency === "NONE") {
-        outcome.innerHTML = "Nie wybrano waluty";
-    } else {
-        outcome.innerHTML = `${amount} ${initialCurrency} = ${result.toFixed(
-            2
-          )} ${finalCurrency}`;
-        }
-      });
+    outcome.innerHTML = "Wybierz walutę";
+};
 
-      
-      buttonClear.addEventListener("click", () => {
-        outcome.innerHTML = "Wybierz walutę";
-      });
+const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", calculation);
+    const buttonClear = document.querySelector(".js-buttonClear");
+    buttonClear.addEventListener("click", cleaning);
+}
+init();
